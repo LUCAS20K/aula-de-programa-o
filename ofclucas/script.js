bbcb15eee73047ba38c8f4189bb1877d470fbec0
LUCAS20K <lucasluna18K@GMAIL.COM>
2023-01-04 18:35:47 -0300

    parte1

--- ofclucas/script.js
+++ ofclucas/script.js
@@ -1,4 +1,26 @@
-const Lucas=24
-const Rocha=06
-console.log(x+y)
-console.log(G-K)
+// const Lucas=24
+// const Rocha=06
+// console.log(Lucas+Rocha)
+// console.log(Lucas-Rocha)
+// console.log(Lucas*Rocha)
+// console.log(Lucas/Rocha)
+// // console.log(Lucas%Rocha)
+
+// const Luna=18
+// const Mateus=20
+// if (Luna>Mateus){
+// console.log(Luna)
+// }else{
+// console.log(Mateus)
+// }
+
+const Black=90
+const White=178
+const Blue=400
+if(Blue===90){
+console.log(Blue=90)
+}else{
+console.log(Blue||Black)
+}else{
+console.log(Blue||White)
+}
